54e1f1ade2dcd46e0fcc0bc954db057b8735f36a
infiniteCable2 <steffen.dahansi.werner@googlemail.com>
2025-02-04 20:16:50 +0100

    Revert "Update safety_volkswagen_meb.h.........................................."
    This reverts commit d4bbe1f0b06211a33918e521e82c1fe827a5ce2c.

--- board/safety/safety_volkswagen_meb.h
+++ board/safety/safety_volkswagen_meb.h
@@ -151,7 +151,7 @@ static void volkswagen_meb_rx_hook(const CANPacket_t *to_push) {
 
       vehicle_moving = (fr > 0U) || (rr > 0U) || (rl > 0U) || (fl > 0U);
 
-      UPDATE_VEHICLE_SPEED(((fr + rr + rl + fl) / 4 ) * 75 / 3.6);
+      UPDATE_VEHICLE_SPEED(((fr + rr + rl + fl) / 4 ) * 75 / 36000);
     }
 
     // Update vehicle yaw rate for curvature checks
