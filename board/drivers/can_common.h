0bdbe5fa46a6e5a0e7b8211b8725e8718c76fb6a
infiniteCable2 <steffen.dahansi.werner@googlemail.com>
2026-03-27 15:03:46 +0100

    ignition

--- board/drivers/can_common.h
+++ board/drivers/can_common.h
@@ -200,6 +200,12 @@ void ignition_can_hook(CANPacket_t *msg) {
       ignition_can = (msg->data[0] >> 5) == 0x6U;
       ignition_can_cnt = 0U;
     }
+	
+	// Volkswagen MEB exception
+    if ((msg->addr == 0x3C0) && (len == 4)) {
+      ignition_can = (msg->data[2] >> 1) & 1U;
+      ignition_can_cnt = 0U;
+    }
 
   }
 }
